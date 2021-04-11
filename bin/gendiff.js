fef7fbc6b7a07e1a5c8db7b21c833079f307ec63
Artur Dolinkin <dolart14@mail.ru>
2021-04-11 12:17:32 +0300

    add arguments description and --format option to help content

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,7 @@ import program from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .arguments('<file1path>, <file2path>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
