deb192c67c6678b58eefc0586e0fd0a3a34e5cb7
Artur Dolinkin <dolart14@mail.ru>
2021-04-12 15:38:27 +0300

    configure eslint and add 'make lint' command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,7 @@ program
   .version('0.0.1')
   .arguments('<file1path> <file2path>')
   .option('-f, --format [type]', 'output format')
+  // eslint-disable-next-line no-console
   .action((file1path, file2path) => console.log(genDiff(file1path, file2path)));
 
 program.parse(process.argv);
