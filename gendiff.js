9802fb7837120ce10da3d0afafb0c82c3e7fe329
Artur Dolinkin <dolart14@mail.ru>
2021-04-12 16:30:11 +0300

    try to reduce Cognitive Complexity for Code Climate

--- gendiff.js
+++ gendiff.js
@@ -4,10 +4,8 @@ import * as fs from 'fs';
 const genDiff = (file1Path, file2Path) => {
   const data1 = JSON.parse(fs.readFileSync(file1Path));
   const data2 = JSON.parse(fs.readFileSync(file2Path));
-
   const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.cloneDeep(keys).sort();
-
   const resultArray = sortedKeys.reduce((acc, key) => {
     if (!_.has(data1, key)) {
       return [...acc, `  + ${key}: ${data2[key]}`];
